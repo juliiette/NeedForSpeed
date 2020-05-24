33b33c6a5e41c8934f2dd097f14b9abe1556bb94
MykolaV <kolya.vasevich@gmail.com>
2020-05-24 19:12:04 +0300

    fixed cash changes output

--- Business.Models/PlayerModel.cs
+++ Business.Models/PlayerModel.cs
@@ -1,13 +1,32 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace Business.Models
 {
-    public class PlayerModel
+    public class PlayerModel : INotifyPropertyChanged
     {
+        private int _cash;
+        
         public int Id { get; set; }
 
         public string Name { get; set; }
         
         public CarModel Car { get; set; }
 
-        public int Cash { get; set; }
+        public int Cash
+        {
+            get => _cash;
+            set
+            {
+                _cash = value;
+                OnPropertyChanged(nameof(Cash));
+            } 
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
